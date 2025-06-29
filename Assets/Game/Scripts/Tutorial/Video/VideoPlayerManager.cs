b40377295d028b1df1b02e092d9533ec1bec0d75
Dima_Zharov <dimakarlenkovv@yandex.ru>
2025-06-29 22:51:52 +0300

    videoclip update

--- Assets/Game/Scripts/Tutorial/Video/VideoPlayerManager.cs
+++ Assets/Game/Scripts/Tutorial/Video/VideoPlayerManager.cs
@@ -5,15 +5,19 @@ using UnityEngine.Video;
 
 public class VideoPlayerManager : MonoBehaviour
 {
-    private const string VIDEO_URL_MAIN = "hhtps://dima-zharov.github.io/Dumplings-invasion-video-host";
-    private List<string> videoUrls = new List<string>() { "BroccoliTutorial.mp4", "CarrotTutorial.mp4", "PotatoTutorial.mp4" };
+    private const string VIDEO_URL_MAIN = "https://dima-zharov.github.io/Dumplings-invasion-video-host/";
+    private List<string> _videoUrls = new List<string>() { "BroccoliTutorial.mp4", "CarrotTutorial.mp4", "PotatoTutorial.mp4" };
+    private bool _isWebGLPlayer;
     [SerializeField] private RawImage _videoRawImage;
     [SerializeField] private Sprite _noInternetConnectSprite;
     [SerializeField] private VideoPlayer _videoPlayer;
     [SerializeField] private TutorialStep[] _steps;
+
     private void Start()
     {
-        _videoPlayer.source = VideoSource.Url;
+        _isWebGLPlayer = Application.platform == RuntimePlatform.WebGLPlayer;
+        if (_isWebGLPlayer)
+            _videoPlayer.source = VideoSource.Url;
         ChoseVideoClip(0);
         PlayVideo();
     }
@@ -21,15 +25,17 @@ public class VideoPlayerManager : MonoBehaviour
     public void PlayVideo()
     {
         _videoPlayer?.Play();
-        if(Application.platform == RuntimePlatform.WebGLPlayer && !_videoPlayer.isPrepared)
+        if (_isWebGLPlayer && !_videoPlayer.isPrepared)
             ShowError();
     }
 
 
     public void ChoseVideoClip(int clipId)
     {
-        if(_videoPlayer.isPrepared)
-            _videoPlayer.url = VIDEO_URL_MAIN + videoUrls[clipId];
+        if (_videoPlayer.isPrepared && _isWebGLPlayer)
+        {
+            _videoPlayer.url = VIDEO_URL_MAIN + _videoUrls[clipId];
+        }
         else
             _videoPlayer.clip = _steps[clipId].VideoClip;
     }
